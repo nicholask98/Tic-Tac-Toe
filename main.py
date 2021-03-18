623661bf8da079adf0db2997b20e8444206703f6
nicholask98 <72052647+nicholask98@users.noreply.github.com>
2021-03-17 19:12:45 -0700

    dun with step one

--- main.py
+++ main.py
@@ -1,63 +1,25 @@
-from time import sleep
+def horizontals(board_size):
+    for hor in range(board_size):
+        print(' ---', end = '')
+    print(' ')
+    return 
 
-def print_board(board):
-    for row in board:
-        for pos, col in enumerate(row):
-            if pos == len(row) - 1:
-                print(col)
-                continue
-            print(col, end = ' | ')
-        print()
+def verticals(board_size):
+    for ver in range(board_size):
+        print('|   ', end = '')
+    print('|')
+    return -1
 
-def win_check():
-    print('FIXME: Win_check')
+def print_board(board_size):
+    for cell in range(board_size):
+        horizontals(board_size)
+        verticals(board_size)
+    horizontals(board_size)
 
-def is_valid(input_list):
-    if board[int(input_list[0])][int(input_list[1])] != ' ':
-        player_input = input('Invalid move: Pick a number (1-9) or 0 to quit\n')
-        player_move(player_input)
-    return True
-
-def player_move(player_input):
-    player_coordinates = move_dict[player_input]
-    input_list = player_coordinates.split()
-    if is_valid(input_list):
-        board[int(input_list[0])][int(input_list[1])] = 'X'
-
-    print_board(board)
-    win_check()
-
-def comp_move():
-    print('FIXME: AI move (comp_move())')
+def main():
+    board_size = int(input('Enter a number for gameboard size:\n'))
+    print_board(board_size)
     
-
-    sleep(1)
-    print_board(board)
-    win_check()
-
-board = [
-    [' ', ' ', ' '],
-    [' ', ' ', ' '],
-    [' ', ' ', ' ']
-]
-
-move_dict = {
-    '1': '0 0',
-    '2': '0 1',
-    '3': '0 2',
-    '4': '1 0',
-    '5': '1 1',
-    '6': '1 2',
-    '7': '2 0',
-    '8': '2 1',
-    '9': '2 2'
-}
-
-
-player_input = input('TIC TAC TOE! Pick a number (1-9)\n')
-while player_input != '0':
-    player_move(player_input)
-    sleep(1)
-    comp_move()
-    player_input = input('Pick a number (1-9) or 0 to quit\n')
+if __name__ == '__main__':
+    main()
 
