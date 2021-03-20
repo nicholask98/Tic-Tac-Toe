f16bd8acc497a8c52c6915985b7244a5b959ecc2
nicholask98 <72052647+nicholask98@users.noreply.github.com>
2021-03-20 16:08:58 -0700

    Update: Win Checks complete

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Tic-Tac-Toe
 
+## Part 1:
+
 Time for some fake graphics! Let’s say we want to draw game boards that look like this:
 
  --- --- --- 
@@ -16,3 +18,22 @@ Ask the user what size game board they want to draw, and draw it for them to the
 Remember that in Python 3, printing to the screen is accomplished by
 
   print("Thing to show on screen")
+
+
+
+
+
+## Part 2:
+
+As you may have guessed, we are trying to build up to a full tic-tac-toe board. However, this is significantly more than half an hour of coding, so we’re doing it in pieces.
+
+Today, we will simply focus on checking whether someone has WON a game of Tic Tac Toe, not worrying about how the moves were made.
+
+If a game of Tic Tac Toe is represented as a list of lists, like so:
+
+game = [[1, 2, 0],
+	[2, 1, 0],
+	[2, 1, 1]]
+where a 0 means an empty square, a 1 means that player 1 put their token in that space, and a 2 means that player 2 put their token in that space.
+
+Your task this week: given a 3 by 3 list of lists that represents a Tic Tac Toe game board, tell me whether anyone has won, and tell me which player won, if any. A Tic Tac Toe win is 3 in a row - either in a row, a column, or a diagonal. Don’t worry about the case where TWO people have won - assume that in every board there will only be one winner.
--- main.py
+++ main.py
@@ -16,10 +16,57 @@ def print_board(board_size):
         verticals(board_size)
     horizontals(board_size)
 
+def vert_win(current_moves, win): # checks for vertical win by either player
+    winner = 0
+    for move in range(len(current_moves[0])):
+        if current_moves[0][move] != 0:
+            if current_moves[0][move] == current_moves[1][move] and current_moves[0][move] == current_moves[2][move]: 
+                win = True
+                winner = current_moves[0][move]
+    return win, winner
+
+def hori_win(current_moves, win):
+    winner = 0
+    for row in range(len(current_moves[0])): # checks for horizontal win by either player
+        current_row = current_moves[row]
+        if current_row[0] != 0:
+            if current_row[0] == current_row[1] and current_row[0] == current_row[2]: 
+                win = True
+                winner = current_row[0]
+    return win, winner
+
+def diag_win(current_moves, win): # checks for diagonal win by either player
+    winner = 0
+    if current_moves[0][0] != 0:
+        if current_moves[0][0] == current_moves[1][1] and current_moves[0][0] == current_moves[2][2]:
+            win = True
+            winner = current_moves[1][1]
+        elif current_moves[0][2] == current_moves[1][1] and current_moves[0][2] == current_moves[2][0]:
+            win = True
+            winner = current_moves[1][1]
+    return win, winner
+
+def win_check(current_moves):
+    win = False
+    win, winner = vert_win(current_moves, win)
+    if win == False:
+        win, winner = hori_win(current_moves, win)
+    if win == False:
+        win, winner = diag_win(current_moves, win)
+    return win, winner
+
 def main():
     board_size = int(input('Enter a number for gameboard size:\n'))
     print_board(board_size)
-    
+    current_moves = [[1, 2, 1], #Sample board for testing
+                     [0, 2, 1],
+                     [2, 2, 1]]
+    win, winner = win_check(current_moves)
+    if win:
+        print('Winner: Player ' + str(winner))
+
+
+
 if __name__ == '__main__':
     main()
 
